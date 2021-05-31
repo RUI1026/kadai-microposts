ed4cba56755d07b2af606603049b7b4c0b6f0abe
rui.morita <mori_morita@hotmail.co.jp>
2021-05-31 06:54:31 +0000

    kadai-micropost-with-favorite-2

--- database/migrations/2021_05_24_231950_create_microposts_table.php
+++ database/migrations/2021_05_24_231950_create_microposts_table.php
@@ -19,6 +19,10 @@ class CreateMicropostsTable extends Migration
             $table->string('content');
             $table->timestamps();
             
+            // user外部キー制約
+            $table->foreign('user_id')->references('id')->on('users');
+            // user外部キー制約
+            $table->foreign('id')->references('micropost_id')->on('favorites_posts');
 
         });
     }
